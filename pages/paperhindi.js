754c62eefb3e2c563236524a70e643be6aaad350
lakshyamahawar14 <83651022+lakshyamahawar14@users.noreply.github.com>
2022-12-11 15:24:46 +0530

    adding Hindi page
    adding hindi page

---
+++ pages/paperhindi.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import AddHindi from '../components/AddHindi'
+import Generate from '../components/Generate'
+import { useRouter } from 'next/router'
+import HeadInfo from '../components/HeadInfo'
+
+export default function Paper(){
+  const router = useRouter()
+  return (
+    <>
+      <Head>
+        <title>पेपर मेकर | Questa</title>
+      </Head>
+
+      <Header>
+
+      </Header>
+
+      <HeadInfo
+      params={router.query.params}
+      >
+
+      </HeadInfo>
+      
+      <AddHindi>
+
+      </AddHindi>
+
+      <Generate>
+        
+      </Generate>
+
+    </>
+  )
+}
