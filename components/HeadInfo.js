7f9beea1b1f198bacb863dd47a0b6a5e70c18e35
lakshyamahawar14 <83651022+lakshyamahawar14@users.noreply.github.com>
2022-12-11 21:30:05 +0530

    refracting PDF
    refracting PDF

--- components/HeadInfo.js
+++ components/HeadInfo.js
@@ -5,15 +5,15 @@ export default function HeadInfo(props) {
         <div className='grid m-2 bg-red-50 place-items-center'>
             <div className='m-2 my-4 flex flex-col justify-between w-3/4 max-w-xl' id='paper-header'>
                 <div className='m-2 flex justify-between'>
-                    <p className='text-lg text-center'>{props.params[0]}</p>
-                    <p className='text-lg text-center'>Name: ..........................</p>
+                    <p className='text-sm'>{props.params[0]}</p>
+                    <p className='text-sm'>Name: ..........................</p>
                 </div>
                 <div className='m-2 flex justify-between'>
-                    <p className='text-lg text-center'>Subject: {props.params[3]}</p>
-                    <p className='text-lg text-center'>Roll No: .....</p>
-                    <p className='text-lg text-center'>{props.params[2]}</p>
+                    <p className='text-sm'>Subject: {props.params[3]}</p>
+                    <p className='text-sm'>Roll No: .....</p>
+                    <p className='text-sm'>{props.params[2]}</p>
                 </div>
-                <p className='text-lg text-center m-4'>{props.params[1]}</p>
+                <p className='text-sm m-4 text-center'>{props.params[1]}</p>
             </div>
         </div>
         
